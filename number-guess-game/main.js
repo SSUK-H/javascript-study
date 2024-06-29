c688d1b52e4e91fa71616224ad4d138e26f878e8
Seongsuk Hong <ssukh77@naver.com>
2024-06-30 02:27:36 +0900

    feat: 5번 기회가 끝나면 게임 종료와 다시하기 버튼 클릭 초기화

--- number-guess-game/main.js
+++ number-guess-game/main.js
@@ -9,12 +9,20 @@
 // 유저가 이미 입력한 숫자를 또 입력하면 알림, 기회를 깍지 않음
 
 let computerNum = 0;
-const playButton = document.getElementById("play-button"); // id가 play-button인 html 태그 선택
+// id가 play-button인 html 태그 선택
+const playButton = document.getElementById("play-button"); // 정답 버튼
 const userInput = document.getElementById("user-input"); // 입력창
 const resultArea = document.getElementById("result-area"); // 정답
+const resetButton = document.getElementById("reset-button"); // 다시하기 버튼
+const chancesArea = document.getElementById("chances-area"); // 기회
+let chances = 5; // 기회 총 3번
+let gameOver = false; // 게임 종료 여부
 
-// 정답 버튼 클릭 시
+// 정답 버튼 클릭 시 게임 진행
 playButton.addEventListener("click", play);
+
+// 다시하기 버튼 클릭 시 게임 초기화
+resetButton.addEventListener("click", reset);
 
 // 랜덤번호
 function pickRandomNum() {
@@ -28,6 +36,11 @@ function play() {
   const userValue = userInput.value; // 입력값
   console.log("입력한 값: ", userValue);
 
+  chances--; // 기회가 1씩 줄어듬
+  chancesArea.textContent = `남은 기회: ${chances}번`;
+  console.log("남은 기회: ", chances);
+
+  // 입력 값에 따라 결과 출력
   if (userValue < computerNum) {
     console.log("Up!!");
     resultArea.textContent = "Up!!";
@@ -38,6 +51,25 @@ function play() {
     console.log("맞췄습니다!");
     resultArea.textContent = "맞췄습니다!";
   }
+
+  // 기회가 끝났을 때 게임 종료
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+// 초기화
+function reset() {
+  // user input창, 결과 초기화
+  userInput.value = "";
+  resultArea.textContent = "정답을 입력해주세요.";
+
+  // 새로운 번호가 생성
+  pickRandomNum();
 }
 
 pickRandomNum();
