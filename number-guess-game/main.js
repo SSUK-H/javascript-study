1b2b1941d7b135402c8d1acacf52fd1efaf8e993
Seongsuk Hong <ssukh77@naver.com>
2024-06-30 02:18:13 +0900

    feat: 정답 버튼 클릭 시 입력 값에 따라 결과 출력

--- number-guess-game/main.js
+++ number-guess-game/main.js
@@ -9,12 +9,35 @@
 // 유저가 이미 입력한 숫자를 또 입력하면 알림, 기회를 깍지 않음
 
 let computerNum = 0;
+const playButton = document.getElementById("play-button"); // id가 play-button인 html 태그 선택
+const userInput = document.getElementById("user-input"); // 입력창
+const resultArea = document.getElementById("result-area"); // 정답
+
+// 정답 버튼 클릭 시
+playButton.addEventListener("click", play);
 
 // 랜덤번호
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
   // Math.random(): 0~ 사이 값을 반환, 1에 가까운 숫자를 반환
   console.log("정답", computerNum);
+}
+
+// 게임 진행
+function play() {
+  const userValue = userInput.value; // 입력값
+  console.log("입력한 값: ", userValue);
+
+  if (userValue < computerNum) {
+    console.log("Up!!");
+    resultArea.textContent = "Up!!";
+  } else if (userValue > computerNum) {
+    console.log("Down!!");
+    resultArea.textContent = "Down!!";
+  } else {
+    console.log("맞췄습니다!");
+    resultArea.textContent = "맞췄습니다!";
+  }
 }
 
 pickRandomNum();
