556890b1c48c090b88aabd4e345bd7a4677ff745
Seongsuk Hong <ssukh77@naver.com>
2024-07-03 18:19:12 +0900

    feat: 체크버튼 클릭 시 작업 완료하기
    - 버튼에 들어가는 아이콘 font-awesome 아이콘으로 변경
    - UI 수정
    - 체크버튼 클릭 시 텍스트에 밑줄,  배경색 회색, 체크표시 진하게 변경해서 완료 처리
    - 완료된 상태일 때 클릭하면 되돌리기

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -8,6 +8,7 @@
 
 const taskForm = document.getElementById("task-form");
 const taskInput = document.getElementById("task-input");
+const checkButton = document.getElementById("#check-button");
 let taskList = [];
 
 // 할일 입력
@@ -17,8 +18,12 @@ function handleSubmit(e) {
   e.preventDefault();
 
   // 입력한 할일 저장
-  const taskContent = taskInput.value;
-  taskList.push(taskContent);
+  const task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
   console.log(taskList);
 
   render(); // 할일 목록에 추가
@@ -32,17 +37,51 @@ function render() {
   let resultHTML = "";
 
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `
-      <div class="task">
+    if (taskList[i].isComplete == true) {
+      resultHTML += `
+      <div class="task task-done">
         <div class="task-content">
-         <button type="button">완료</button>
-         <p>${taskList[i]}</p>
+          <button id="check-button" class="checked" type="button" onclick="toggleComplete('${taskList[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+         <p class="task-done">${taskList[i].taskContent}</p>
         </div>
-        <button type="button">삭제</button>
+        <button id="delete-button" type="button"><i class="fa-regular fa-trash-can"></i></button>
       </div>
     `;
+    } else {
+      resultHTML += `
+      <div class="task">
+        <div class="task-content">
+          <button id="check-button" type="button" onclick="toggleComplete('${taskList[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+         <p>${taskList[i].taskContent}</p>
+        </div>
+        <button id="delete-button" type="button"><i class="fa-regular fa-trash-can"></i></button>
+      </div>
+    `;
+    }
   }
-  console.log(resultHTML);
 
   taskBoard.innerHTML = resultHTML;
+}
+
+// 작업 완료하기
+// 체크한 작업 정보 가져와서 체크 여부 확인
+function toggleComplete(id) {
+  const task = taskList.find((task) => task.id === id);
+
+  if (task) {
+    task.isComplete = !task.isComplete;
+  }
+  console.log("click: ", id, taskList);
+
+  render(); // UI에 반영
+}
+
+// 교유 아이디 만들기
+// 어떤 작업인지 확인하기 위해
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
