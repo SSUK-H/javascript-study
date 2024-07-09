fd839640f782a73af7aac7023a92294d0888c4b7
Seongsuk Hong <ssukh77@naver.com>
2024-07-09 20:48:05 +0900

    feat: news api 호출 및 데이터 저장

---
+++ ssuk-times/main.js
@@ -0,0 +1,15 @@
+const API_KEY = `20d1a87375b944aabce0c52c8cc088db`;
+let news = [];
+
+const getLatestNews = async () => {
+  // URL 인스턴스를 활용해서 api 주소를 만듬
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles;
+  console.log("response", news);
+};
+
+getLatestNews();
