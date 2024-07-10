c6d42657d8229e7f187e31027574c14c670454c9
Seongsuk Hong <ssukh77@naver.com>
2024-07-10 23:39:38 +0900

    design: nav css 수정

--- ssuk-times/main.js
+++ ssuk-times/main.js
@@ -52,12 +52,13 @@ const render = () => {
 getLatestNews();
 
 const openNav = () => {
-  document.getElementById("menu").style.width = "250px";
+  // document.getElementById("menu").style.width = "250px";
+  document.getElementById("menu").style.display = "block";
   document.getElementById("menu").style.marginLeft = "0px";
 };
 
 const closeNav = () => {
-  document.getElementById("menu").style.width = "0";
+  // document.getElementById("menu").style.width = "0";
   document.getElementById("menu").style.marginLeft = "-250px";
 };
 
