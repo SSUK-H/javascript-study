740426a94f02d35dea5814184b63cfce4f67f1c3
홍성숙 <134491629+SSUK-H@users.noreply.github.com>
2024-07-15 13:26:43 +0900

    Revert "feat: 첫 페이지일 경우 맨앞, 이전 버튼 제거, 마지막 페이지일 경우 맨뒤, 다음 버튼 제거"
    This reverts commit 6f60387d4cde35617b3b3875c55bf456eb66ed2a.

--- ssuk-times/main.js
+++ ssuk-times/main.js
@@ -180,23 +180,18 @@ const paginationRender = () => {
     lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
 
   // 페이지 버튼 생성
-  let paginationHTML = ``;
-
-  // 첫번째 페이지인 경우 보이지 않기
-  if (page > 1) {
-    paginationHTML += `
+  let paginationHTML = `
     <li class="page-item first-page-button" onclick="moveToPage(1)">
-      <a class="page-link" aria-label="Previous">
+      <a class="page-link" href="#" aria-label="Previous">
         <span aria-hidden="true">&lt;&lt;</span>
        </a>
     </li>
     <li class="page-item prev-button" onclick="moveToPage(${page - 1})">
-      <a class="page-link" aria-label="Previous">
+      <a class="page-link" href="#" aria-label="Previous">
         <span aria-hidden="true">&lt;</span>
       </a>
     </li>
   `;
-  }
 
   for (let i = firstPage; i <= lastPage; i++) {
     paginationHTML += `
@@ -206,21 +201,18 @@ const paginationRender = () => {
     `;
   }
 
-  // 마지막 페이지인 경우 보이지 않기
-  if (page < totalPages) {
-    paginationHTML += `
+  paginationHTML += `
     <li class="page-item next-button" onclick="moveToPage(${page + 1})">
-      <a class="page-link" aria-label="Next">
+      <a class="page-link" href="#" aria-label="Next">
         <span aria-hidden="true">&gt;</span>
       </a>
     </li>
     <li class="page-item last-page-button" onclick="moveToPage(${totalPages})">
-      <a class="page-link" aria-label="Next">
+      <a class="page-link" href="#" aria-label="Next">
         <span aria-hidden="true">&gt;&gt;</span>
       </a>
     </li>
   `;
-  }
 
   document.querySelector(".pagination").innerHTML = paginationHTML;
 };
