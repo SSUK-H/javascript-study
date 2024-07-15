08e3b890a25934c558a65d2abe559095817d3f34
Seongsuk Hong <ssukh77@naver.com>
2024-07-15 14:24:27 +0900

    기존 삭제된 main 파일들 push

---
+++ number-guess-game/main.js
@@ -0,0 +1,120 @@
+// 랜덤번호 지정
+// 유저가 번호를 입력한다 그리고 go 라는 버튼을 누름
+// 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
+// 랜덤번호가 < 유저번호 Down!!
+// 랜덤번호가 > 유저번호 Up!!
+// Reset버튼을 누르면 게임이 리셋된다.
+// 5번의 기회를 다쓰면 게임이 끝남 (더이상 추측 불가, 버튼이 disable )
+// 유저가 1~100 범위 밖에 숫자를 입력하면 알림, 기회를 깍지 않음
+// 유저가 이미 입력한 숫자를 또 입력하면 알림, 기회를 깍지 않음
+
+let computerNum = 0;
+// id가 play-button인 html 태그 선택
+const playButton = document.getElementById("play-button"); // 정답 버튼
+const userInput = document.getElementById("user-input"); // 입력창
+const resultArea = document.getElementById("result-area"); // 정답
+const resetButton = document.getElementById("reset-button"); // 다시하기 버튼
+const chancesArea = document.getElementById("chances-area"); // 기회
+let chances = 3; // 기회 총 3번
+let gameOver = false; // 게임 종료 여부
+let history = [];
+
+// 정답 버튼 클릭 시 게임 진행
+playButton.addEventListener("click", play);
+
+// 다시하기 버튼 클릭 시 게임 초기화
+resetButton.addEventListener("click", reset);
+
+// 재입력시 기존값 초기화
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
+
+// 랜덤번호
+function pickRandomNum() {
+  computerNum = Math.floor(Math.random() * 100) + 1;
+  // Math.random(): 0~ 사이 값을 반환, 1에 가까운 숫자를 반환
+  console.log("정답", computerNum);
+}
+
+// 게임 진행
+function play() {
+  const userValue = parseInt(userInput.value); // 입력값을 정수로 변환
+  console.log("입력한 값: ", userValue);
+
+  // 유효성 검사
+  // 1~100 사이 숫자가 아닌 경우
+  if (isNaN(userValue) || userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과 100사이의 유효한 숫자를 입력해주세요.";
+    return;
+  }
+
+  // 이미 입력한 숫자인 경우
+  if (history.includes(userValue)) {
+    resultArea.textContent = "이미 입력한 숫자입니다.";
+    return;
+  }
+
+  chances--; // 기회가 1씩 줄어듬
+  chancesArea.textContent = `남은 입력 횟수: ${chances}회`;
+  console.log("남은 입력 횟수: ", chances);
+
+  // 입력한 값 저장
+  history.push(userValue);
+  console.log("history", history);
+
+  // 입력 값에 따라 결과 출력
+  if (userValue < computerNum) {
+    console.log("Up!!");
+    resultArea.textContent = `힌트: Up!! (정답: ${computerNum})`;
+    userInput.placeholder =
+      "숫자 " +
+      history.join(", ") +
+      "을(를) 제외한 1과 100사이의 유효한 숫자를 입력해주세요.";
+  } else if (userValue > computerNum) {
+    console.log("Down!!");
+    resultArea.textContent = `힌트: Down!! (정답: ${computerNum})`;
+    userInput.placeholder =
+      "숫자 " +
+      history.join(", ") +
+      "을(를) 제외한 1과 100사이의 유효한 숫자를 입력해주세요.";
+  } else {
+    console.log("맞췄습니다!");
+    resultArea.textContent = "맞췄습니다!";
+    userInput.placeholder = "";
+    gameOver = true; // 게임 종료
+  }
+
+  // 기회가 끝났을 때 게임 종료
+  if (chances < 1) {
+    gameOver = true;
+
+    // 마지막 기회에도 못 맞췄을 때
+    if (userValue !== computerNum) {
+      resultArea.textContent = "더 이상 프로그램을 실행할 수 없습니다.";
+      userInput.placeholder = "";
+    }
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+// 초기화
+function reset() {
+  // user input창, 결과 초기화
+  chances = 3;
+  resultArea.textContent = "정답을 입력해주세요.";
+  chancesArea.textContent = `남은 기회: ${chances}번`;
+  userInput.placeholder = "1과 100사이의 유효한 숫자를 입력해주세요.";
+  userInput.value = "";
+  history = [];
+  gameOver = false;
+  playButton.disabled = false;
+
+  // 새로운 번호가 생성
+  pickRandomNum();
+}
+
+pickRandomNum();
---
+++ to-do-list/main.js
@@ -0,0 +1,151 @@
+// 유저가 값을 입력한다
+// + 버튼을 클릭하면, 할 일이 추가된다
+// 삭제 버튼을 누르면 할 일이 삭제된다
+// 완료 버튼을 누르면 할 일이 종료되고 밑줄이 긋기
+// 종료 탭을 누르면 언더바 이동
+// 종료 탭은 종료된 할 일만, 진행 중인 탭은 진행 중인 할 일만
+// 전체 탭은 전체 할 일만
+
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("task-input");
+const checkButton = document.getElementById("#check-button");
+const tabs = document.querySelectorAll(".task-tabs li");
+let taskList = [];
+let filterList = [];
+let mode = "all";
+
+// 페이지 로드 시 입력창 자동 포커스
+taskInput.focus();
+
+// 할일 입력
+taskForm.addEventListener("submit", addTask);
+
+// 진행 중 / 완료 할일 목록 필터링
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function (e) {
+    // 선택된 탭 표시
+    tabs.forEach((tab) => {
+      tab.classList.remove("action"); // 모든 탭에서 action 클래스 제거
+    });
+    e.currentTarget.classList.add("action"); // 클릭한 탭에 action 클래스 추가
+
+    filterTask(e); // 할일 목록 필터링
+  });
+});
+
+// 작업 추가하기
+function addTask(e) {
+  e.preventDefault();
+
+  // 입력값 없는 경우 알림
+  if (taskInput.value === "") {
+    alert("할 일을 입력해주세요.");
+    return;
+  }
+
+  // 입력한 할일 저장
+  const task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  console.log("작업 추가:", taskList);
+
+  filterTask(); // 데이터 필터링, UI에 반영
+
+  taskInput.value = ""; // 저장 후 초기화
+}
+
+// 할일 목록에 나타내기
+function render() {
+  const taskBoard = document.getElementById("task-board");
+  // 작업 상태 분류에 따라 작업 데이터 분류
+  let list = mode === "all" ? taskList : filterList;
+  let resultHTML = "";
+
+  // 작업 리스트 출력
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `
+      <div class="task task-done">
+        <div class="task-content">
+          <button id="check-button" class="checked" type="button" onclick="toggleComplete('${taskList[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+         <p class="task-done">${list[i].taskContent}</p>
+        </div>
+        <button id="delete-button" type="button" onclick="deleteTask('${list[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
+      </div>
+    `;
+    } else {
+      resultHTML += `
+      <div class="task">
+        <div class="task-content">
+          <button id="check-button" type="button" onclick="toggleComplete('${list[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+         <p>${list[i].taskContent}</p>
+        </div>
+        <button id="delete-button" type="button" onclick="deleteTask('${list[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
+      </div>
+    `;
+    }
+  }
+
+  taskBoard.innerHTML = resultHTML;
+}
+
+// 작업 완료하기
+// 체크한 작업 정보 가져와서 체크 여부 확인
+function toggleComplete(id) {
+  const task = taskList.find((task) => task.id === id);
+
+  if (task) {
+    task.isComplete = !task.isComplete;
+  }
+  console.log("click: ", id, taskList);
+
+  filterTask(); // 데이터 필터링, UI에 반영
+}
+
+// 삭제하기
+function deleteTask(id) {
+  // 선택한 아이디인 배열 인덱스 찾기
+  const taskIndex = taskList.findIndex((task) => task.id === id);
+  console.log(taskIndex);
+
+  // 해당 아이디가 포함된 배열 요소를 삭제
+  if (taskIndex !== -1) {
+    taskList.splice(taskIndex, 1);
+  }
+
+  console.log("click: ", id, taskList);
+
+  filterTask(); // 데이터 필터링, UI에 반영
+}
+
+// 할일 목록 필터링
+function filterTask(e) {
+  if (e) {
+    console.log("filter:", e.target); // 클릭한 tab 정보
+    mode = e.target.id;
+  }
+
+  // 작업 상태에 따라 작업 리스트 분리
+  if (mode === "ongoing") {
+    filterList = taskList.filter((task) => !task.isComplete);
+    console.log("진행 중:", filterList);
+  } else if (mode === "done") {
+    filterList = taskList.filter((task) => task.isComplete);
+    console.log("완료:", filterList);
+  }
+
+  render();
+}
+
+// 교유 아이디 만들기
+// 어떤 작업인지 확인하기 위해
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
