6c3ea1e7a0c15b2ebafbab2619958d8e74839f30
홍성숙 <134491629+SSUK-H@users.noreply.github.com>
2024-07-15 13:26:43 +0900

    Revert "feat: api 호출 url 변경,  UI 추가"
    This reverts commit c6970fdf884b2c92f253e3ae4de5fb5058401d1f.

--- ssuk-times/main.js
+++ ssuk-times/main.js
@@ -1,33 +1,10 @@
 const API_KEY = `20d1a87375b944aabce0c52c8cc088db`;
 let news = [];
 
-const openNav = () => {
-  document.getElementById("menu").style.width = "250px";
-  document.getElementById("menu").style.marginLeft = "0px";
-};
-
-const closeNav = () => {
-  document.getElementById("menu").style.width = "0";
-  document.getElementById("menu").style.marginLeft = "-250px";
-};
-
-const openSearch = () => {
-  const search = document.getElementById("search-form");
-
-  if (search.style.display === "none") {
-    search.style.display = "flex";
-    search.style.opacity = "1";
-  } else {
-    search.style.display = "none";
-    search.style.opacity = "0";
-  }
-};
-
 const getLatestNews = async () => {
   // URL 인스턴스를 활용해서 api 주소를 만듬
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
-    // `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${API_KEY}`
+    `https://ssuk-times.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
